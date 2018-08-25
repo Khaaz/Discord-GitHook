486466bf08f0815eedc497aa003bafed365f82f4
Khaazz <lbellec@live.com>
2018-08-25 16:51:20 +0200

    Changed the rate limit handling in an external file which handle all webhooks post request

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,22 @@
 'use strict';
 
 // Dependency
-const crypto = require('crypto');
+const { createHmac, timingSafeEqual } = require('crypto');
 
 // Others
 const config = require('../../configs/config.json');
 
 const verifySignature = function verifySignature(signature, payloadBody) {
-    const HMAC = `sha1=${crypto.createHmac('sha1', config.authorization)
+    const HMAC = `sha1=${createHmac('sha1', config.authorization)
         .update(JSON.stringify(payloadBody))
         .digest('hex')}`;
-    return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(HMAC));
+    return timingSafeEqual(Buffer.from(signature), Buffer.from(HMAC));
 };
+
+/** Wait for a delay in ms*/
+const sleep = (ms) => new Promise((res) => setTimeout(() => res(), ms));
 
 module.exports = {
     verifySignature,
+    sleep,
 };
