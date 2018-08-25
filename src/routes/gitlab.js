1f6e5344d590323727369e83f99c7dba39e0908d
Khaazz <lbellec@live.com>
2018-08-25 22:29:50 +0200

    gitlab endpoint

--- src/routes/gitlab.js
+++ src/routes/gitlab.js
@@ -1,16 +1,12 @@
 'use strict';
 
-// Dependency
-const superagent = require('superagent');
-
 // Others
-const webhooks = require('../../configs/webhooks.json');
-const config = require('../../configs/config.json');
+// const config = require('../../configs/config.json');
 
 // Const
-const discord = 'https://discordapp.com/api';
 
 const gitlab = (req, res) => {
+    res.send('Success!');
     console.log(req);
 };
 
